cf8082d70d4f196d98c65b4462ac1f0294e97cd2
yaobisi <320130049@qq.com>
2022-07-23 18:09:53 +0800

    refactor(hooks): 添加hooks

---
+++ src/hooks/useMousePosition.ts
@@ -0,0 +1,32 @@
+import {
+  onBeforeUnmount,
+  onMounted,
+  reactive
+} from 'vue'
+
+export default function () {
+  const client = reactive({
+    x: 0,
+    y: 0
+  })
+  const page = reactive({
+    x: 0,
+    y: 0
+  })
+  const clickHandler = (event: MouseEvent)=>{
+    client.x = event.clientX
+    client.y = event.clientY
+    page.x = event.pageX
+    page.y = event.pageY
+  }
+  onMounted(()=>{
+    window.addEventListener('click', clickHandler)
+  })
+  onBeforeUnmount(()=>{
+    window.removeEventListener('click', clickHandler)
+  })
+  return {
+    client,
+    page
+  }
+}
